c7d4c7a14b13b080e3d8420c3f1827ea1c0e5fc4
Diego Reyes <diegorele13@gmail.com>
2026-08-17 17:47:19 -0500

    feat(routes): index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,19 @@
 import express, { Application } from "express";
+import usersRouter from "./users.routes";
 import erpRouter from "./erp.routes";
 import healthRouter from "./health.routes";
+import authRouter from "./auth.routes";
+import cobrosRouter from "./cobros.routes";
 
 function routerApi(app: Application) {
   const router = express.Router();
   app.use("/api", router);
 
   router.use("/health", healthRouter);
+  router.use("/auth", authRouter);
   router.use("/erp", erpRouter);
+  router.use("/users", usersRouter);
+  router.use("/cobros", cobrosRouter);
 }
 
 export default routerApi;
